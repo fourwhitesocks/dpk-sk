63cc13ead0b459294f05b4ee357fc3f24e768cac
amy stout <asbilly92@gmail.com>
2023-02-03 15:29:22 -0500

    Starting digitalpainkillers.com site initial push

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Digital Pain Killers dot com</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+	p {
+		a {
+			color: red;
+		}
+	}
+</style>
