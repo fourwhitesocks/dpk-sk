5756019cb414bc1ad9bca0c4739042d6c96a0759
amy stout <asbilly92@gmail.com>
2023-02-07 16:04:28 -0500

    working on hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,20 @@
 <div class="grid grid-cols-2 card-container">
-	<img src="./funny-owl-sq-ed.jpg" width="450px" alt="funny owl" />
+	<div class="left-card">
+		<img class="" src="./funny-owl-sq-ed.jpg" width="" alt="funny owl" />
+		<div class="card-bg" />
+	</div>
 
-	<div>
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga incidunt molestias perspiciatis,
-		voluptate iste ratione, enim dolorum magni earum recusandae odio aperiam corrupti consequatur
-		itaque culpa ipsam tempora unde quisquam cupiditate delectus natus voluptatem. Ut illo nobis
-		iusto repudiandae cumque inventore id blanditiis illum quis commodi consectetur autem magni
-		maiores, nesciunt fuga perferendis excepturi! Similique, deserunt sed, tempora voluptatum iure
-		inventore excepturi voluptas quas unde iusto cum repellendus qui, eligendi nesciunt laboriosam
-		ea nulla explicabo!
+	<div class="flex right-card">
+		<div class="card-bg" />
+		<div class="inside-body">
+			<div class="center card-text-medium">
+				<span class="card-text-large">WHO</span><br />
+				needs help?
+			</div>
+			<div class="center card-text-small">Looking for a solution?</div>
+			<div class="center card-text-small">Some customer service?</div>
+			<div class="center card-text-small">A new website?</div>
+		</div>
 	</div>
 </div>
 
@@ -17,6 +23,39 @@
 		a {
 			color: red;
 		}
+	}
+
+	.center {
+		text-align: center;
+	}
+	.inside-body {
+		padding: var(--em-size-8);
+	}
+
+	.card-bg {
+		position: absolute;
+		background-color: lime;
+		transform: rotate(-6deg) skewX(0deg) skewY(0deg) scaleY(1.1) scaleX(1);
+
+		left: 0;
+		right: 0;
+		bottom: 0;
+		top: 0;
+		border-radius: 25px;
+		z-index: -2;
+	}
+
+	.left-card {
+		position: relative;
+	}
+	.right-card {
+		/*padding: var(--em-size-32); */
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-around;
+		background-color: var(--clr-lite-accent);
+		border-radius: 25px;
+		position: relative;
 	}
 
 	.card-container {
@@ -28,5 +67,20 @@
 
 	img {
 		border-radius: 25px;
+		position: relative;
+	}
+
+	.card-text-large {
+		font-weight: bold;
+		font-size: 8rem;
+	}
+
+	.card-text-medium {
+		font-size: var(--fs-700);
+		font-weight: bold;
+	}
+
+	.card-text-small {
+		font-size: var(--fs-600);
 	}
 </style>
